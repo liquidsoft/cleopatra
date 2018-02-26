6763b8cc8dd965f6a569093ce827d04feb1518df
Robert Ontiu <ontiu.robert@gmail.com>
2018-02-26 13:05:01 +0100

    added pipe interceptors

--- example/example.js
+++ example/example.js
@@ -3,27 +3,46 @@
 
 const {createPipeline} = require("./../lib");
 
-/*
- -------------------------------
- Initialize pipelines
- -------------------------------
- */
+/////////////////////////////////
+// Intercepted pipeline
+/////////////////////////////////
 
-const goodbyePipeline = createPipeline().pipe((payload) => {
-	console.log("Goodbye!");
-});
+class MyPipe {
+	runStuff(payload) {
+		console.log("My pipe runs stuff");
+		return payload;
+	}
+}
 
-const examplePipeline = createPipeline().pipe((payload) => {
+const myPipeInterceptor = (pipe) => {
+	return pipe instanceof MyPipe ?
+		payload => pipe.runStuff(payload) :
+		pipe;
+};
 
-	console.log(payload);
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve();
+const interceptedPipeline = createPipeline().intercept(myPipeInterceptor).pipe(new MyPipe());
 
-		}, 2500);
+/////////////////////////////////
+// Sample pipeline with report and throw
+/////////////////////////////////
+
+const examplePipeline = createPipeline()
+	.report(error => console.log(error))
+	.pipe((payload) => {
+		console.log(payload);
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				resolve(payload);
+
+			}, 2500);
+		});
+
+	})
+	.pipe(interceptedPipeline)
+	.capture(payload => {
+		console.log("Done.")
+		return payload;
 	});
-
-}).pipe(goodbyePipeline);
 
 /*
  -------------------------------
